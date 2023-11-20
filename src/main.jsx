c12a2ec83800f1dc8894ef5dd325ca09fa99dc16
Alexandre <alexandre_viegas@icloud.com>
2023-11-20 09:28:40 +0100

    ➕ React router dom implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Accueil from "./pages/Accueil.jsx";
+import Compétences from "./pages/Compétences.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
+import Contact from "./pages/Contact.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Accueil />,
+  },
+  {
+    path: "/competences",
+    element: <Compétences />,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Accueil />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
