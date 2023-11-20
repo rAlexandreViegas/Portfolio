e06d9e274d52fbdf92f36837d34e6eecaa2f2878
Alexandre <alexandre_viegas@icloud.com>
2023-11-20 09:12:48 +0100

    🎉 Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Accueil from "./pages/Accueil.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Accueil />
+  </React.StrictMode>
+);
