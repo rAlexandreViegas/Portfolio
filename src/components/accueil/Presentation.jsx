adfca669343706ebfd1b27e522558c36f6d6cadd
Alexandre <alexandre_viegas@icloud.com>
2023-11-24 10:19:04 +0100

    💄 Add content in Home page

--- src/components/accueil/Presentation.jsx
+++ src/components/accueil/Presentation.jsx
@@ -1,21 +1,58 @@
 import { Avatar } from "@mui/material";
 import avatarImg from "../../assets/avatar.png";
+import links from "../../utils/data/links";
+import stack from "../../utils/data/stack";
+
 export default function Presentation() {
   return (
     <section className="presentation">
-      <div className="presentation__container">
-        <h1 className="presentation__title presentation__title--highlight">
-          Bienvenue, <br />
-          Je suis Alexandre Viegas
-        </h1>
-        <h2 className="presentation__title">Développeur Web Full Stack</h2>
-        <p className="presentation__description">
-          Actuellement en formation en tant que Concepteur Développeur
-          d'Applications, je suis passionné par l'écosystème JavaScript, avec
-          une expertise grandissante dans la stack MERN/SERN.
-        </p>
+      <div className="presentation__wrapper">
+        <div className="presentation__container">
+          <h1 className="presentation__title-h1">
+            Bienvenue, <br />
+            Je suis Alexandre Viegas
+          </h1>
+          <h2 className="presentation__title-h2">Développeur Web Full Stack</h2>
+          <p className="presentation__description">
+            Actuellement en formation en tant que Concepteur Développeur
+            d'Applications, je suis passionné par l'écosystème JavaScript, avec
+            une expertise grandissante dans la stack MERN.
+          </p>
+          <div className="presentation__social-links-container">
+            {links.map((link) => (
+              <a
+                key={link.name}
+                className="presentation__social-link"
+                href={link.link}
+                target="_blank"
+              >
+                <Avatar className="presentation__social-frame" variant="square">
+                  <link.icon className="presentation__social-icon" />
+                  {/* {link.placeholder} */}
+                </Avatar>
+              </a>
+            ))}
+          </div>
+        </div>
+        <Avatar className="presentation__avatar" src={avatarImg} alt="avatar" />
       </div>
-      <Avatar className="presentation__avatar" src={avatarImg} alt="avatar" />
+      <div className="presentation__stack-container">
+        {stack.map((item) => (
+          <a
+            key={item.name}
+            className="presentation__stack-item"
+            href={item.link}
+            target="_blank"
+          >
+            {item.name}
+            <img
+              className="presentation__stack-icon"
+              src={item.icon}
+              alt={item.name}
+            />
+          </a>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/utils/data/links.js
@@ -0,0 +1,26 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+
+const link = [
+  {
+    name: "LinkedIn",
+    icon: LinkedInIcon,
+    placeholder: "in/alexandre-pereira-viegas",
+    link: "https://www.linkedin.com/in/alexandre-pereira-viegas/",
+  },
+  {
+    name: "GitHub",
+    icon: GitHubIcon,
+    placeholder: "rAlexandreViegas",
+    link: "https://github.com/rAlexandreViegas",
+  },
+  {
+    name: "Email",
+    icon: EmailIcon,
+    placeholder: "alexandre_viegas@icloud.com",
+    link: "mailto: alexandre_viegas@icloud.com",
+  },
+];
+
+export default link;
