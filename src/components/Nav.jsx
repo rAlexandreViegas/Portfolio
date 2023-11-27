ef0beb864b7c05d94e6acaf0562ed14a9297823d
Alexandre <alexandre_viegas@icloud.com>
2023-11-27 14:39:56 +0100

    💄 Add media queries

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,68 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button, Menu, MenuItem } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import navLinks from "../utils/data/navLinks";
 
 export default function Nav() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <header className="header">
-      <Link to="/">
+      <Link className="header__link" to="/">
         <h1 className="header__title">
-          <span className="header__title header__title--highlight">
+          <span className="header__title header__title-logo">
             &lt;&#47;&gt;
           </span>
-          Alexandre Viegas
+          <span className="header__title header__title-full">
+            Alexandre Viegas
+          </span>
+          <span className="header__title header__title-short">Alexandre</span>
         </h1>
       </Link>
       <nav className="header__nav">
         <ul className="header__list">
           {navLinks.map((link) => (
-            <li key={link.to} className="header__link">
+            <li key={link.to} className="header__list-link">
               <Link to={link.to}>{link.text}</Link>
             </li>
           ))}
         </ul>
+
+        {/* Mobile */}
+        <div className="header__mobile">
+          <Button
+            className="header__mobile-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </Button>
+          <Menu
+            className="header__mobile-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            {navLinks.map((link) => (
+              <MenuItem key={link.to} className="header__link">
+                <Link to={link.to}>{link.text}</Link>
+              </MenuItem>
+            ))}
+          </Menu>
+        </div>
       </nav>
     </header>
   );
---
+++ src/utils/data/accueil/links.js
@@ -0,0 +1,23 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+
+const link = [
+  {
+    name: "LinkedIn",
+    icon: LinkedInIcon,
+    link: "https://www.linkedin.com/in/alexandre-pereira-viegas/",
+  },
+  {
+    name: "GitHub",
+    icon: GitHubIcon,
+    link: "https://github.com/rAlexandreViegas",
+  },
+  {
+    name: "Email",
+    icon: EmailIcon,
+    link: "mailto: alexandre_viegas@icloud.com",
+  },
+];
+
+export default link;
